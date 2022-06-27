c0871a5ac45078286bd93d843b559cb3b518ffad
paoloBugarin <paolopaolobugarin@gmail.com>
2022-06-27 00:13:26 -0700

    fixed heroku domain

--- src/variables.js
+++ src/variables.js
@@ -1,2 +1,2 @@
-export const domain = "http://localhost:3001";
-//  const domain = "https://simple-crud-react-mysql.herokuapp.com";
+// export const domain = "http://localhost:3001";
+ export const domain = "https://simple-crud-react-mysql.herokuapp.com";

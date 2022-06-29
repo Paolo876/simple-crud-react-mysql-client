f10537e4fe077e5cbf53e14390ac976824825f38
paoloBugarin <paolopaolobugarin@gmail.com>
2022-06-29 00:34:45 -0700

    updated variables.js file, domain url

--- src/variables.js
+++ src/variables.js
@@ -1,3 +1,7 @@
-export const domain = "http://localhost:3001";
+let domain = "https://simple-crud-react-mysql.herokuapp.com";
+if(!process.env.DOMAIN_URL) domain = "http://localhost:3001";
+
+export { domain };
+// export const domain = "http://localhost:3001";
 // export const domain = "https://simple-crud-react-mysql.herokuapp.com";
 // 

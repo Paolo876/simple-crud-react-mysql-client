58978711577f2283c41ef854bfbeccb214688d04
paoloBugarin <paolopaolobugarin@gmail.com>
2023-01-05 00:24:12 -0800

    updated login page ui

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 import { domain } from '../../variables';
 import useAuthActions from '../../hooks/useAuthActions';
 import PageContainer from '../../components/PageContainer/PageContainer';
-import { Container, Typography, Paper, Button, TextField, Alert } from '@mui/material';
+import { Container, Typography, Paper, Button, TextField, Alert, FormControl, InputLabel, OutlinedInput, InputAdornment, IconButton } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LoginIcon from '@mui/icons-material/Login';
 import KeyIcon from '@mui/icons-material/Key';
@@ -13,7 +16,7 @@ export default function Login() {
     const { login, isLoading, error } = useAuthActions();
     const [ username, setUsername ] = useState('');
     const [ password, setPassword ] = useState('');
-
+    const [ showPassword, setShowPassword ] = useState(false)
     const handleSubmit = (e) => {
         e.preventDefault();
         login(`${domain}/auth/login`, { username, password });
@@ -35,16 +38,29 @@ export default function Login() {
                             required
                             sx={{my:2, minWidth: "320px"}}
                         />
-                        <TextField 
-                            id="password" 
-                            type="password" 
-                            label={<p><KeyIcon style={{verticalAlign:"middle"}} sx={{mr: 1}}/>Password</p>} 
-                            value={password} 
-                            variant="outlined" 
-                            onChange={e => setPassword(e.target.value)}
-                            required
-                            sx={{my:2, minWidth: "320px"}}
-                        />
+                        <FormControl sx={{my:2, minWidth: "320px"}} variant="outlined">
+                            <InputLabel htmlFor="outlined-adornment-password"><p><KeyIcon style={{verticalAlign:"middle"}} sx={{mr: 1}}/>Password</p></InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                type={showPassword ? 'text' : 'password'}
+                                value={password} 
+                                onChange={e => setPassword(e.target.value)}
+                                required
+                                endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={() => setShowPassword(prevState => !prevState)}
+                                    onMouseDown={e => e.preventDefault()}
+                                    edge="end"
+                                    >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                                }
+                                label={<p><KeyIcon style={{verticalAlign:"middle"}} sx={{mr: 1}}/>Password</p>}
+                            />
+                        </FormControl>
                         {error && <Alert severity="error">{error}</Alert>}
                         {!isLoading && <Button variant="contained" type="submit" size="large" sx={{ mt: 5 }}>Login</Button>}
                         {isLoading && <Button variant="contained" type="submit" size="large" sx={{ mt: 5 }} disabled>LOGGING IN...</Button>}

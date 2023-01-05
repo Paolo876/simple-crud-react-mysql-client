181ada862848369eb743046ae428d2cbb27f3069
paoloBugarin <paolopaolobugarin@gmail.com>
2023-01-04 22:50:59 -0800

    updated login page ui

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { domain } from '../../variables';
-// import "./Login.scss";
 import useAuthActions from '../../hooks/useAuthActions';
 import PageContainer from '../../components/PageContainer/PageContainer';
-import { Container, Typography, Paper, Button, FormControl, InputLabel, Input, TextField } from '@mui/material';
-import LoginIcon from '@mui/icons-material/Login';
+import { Container, Typography, Paper, Button, TextField, Alert } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LoginIcon from '@mui/icons-material/Login';
 import KeyIcon from '@mui/icons-material/Key';
 
 export default function Login() {
-    const { login, isLoading, error} = useAuthActions();
-    const navigate = useNavigate();
+    const { login, isLoading, error } = useAuthActions();
     const [ username, setUsername ] = useState('');
     const [ password, setPassword ] = useState('');
 
@@ -23,36 +21,33 @@ export default function Login() {
     <PageContainer>
         <Container sx={{display: 'flex', justifyContent:'center', alignItems: "center", height: "70%"}}>
             <Paper className='login-page' sx={{py: 5, px: {xs: 2, md:8}, width: "fit-content", mx: "auto"}} elevation={4}>
-            <Typography variant="h4" fontWeight={700} mb={4} letterSpacing={3}><LoginIcon style={{verticalAlign:"middle"}} sx={{mr: 1}} fontSize="large"/> LOGIN</Typography>
-                {/* <h3>LOGIN</h3> */}
-                <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column", my: 5}}>
-                    <TextField 
-                        id="username" 
-                        type="text" 
-                        label={<p><AccountCircleIcon 
-                        style={{verticalAlign:"middle"}} sx={{mr: 1}}/> Username</p>} 
-                        value={username} 
-                        variant="outlined" 
-                        onChange={e => setUsername(e.target.value)}
-                        required
-                        sx={{my:2}}
-                    />
-                    <TextField 
-                        id="password" 
-                        type="password" 
-                        label={<p><KeyIcon 
-                        style={{verticalAlign:"middle"}} sx={{mr: 1}}/> Password</p>} 
-                        value={password} 
-                        variant="outlined" 
-                        onChange={e => setPassword(e.target.value)}
-                        required
-                        sx={{my:2}}
-                    />
-                    {error && <p className="error">{error}</p>}
-                    {!isLoading && <button className='submit-btn'>LOGIN</button>}
-                    {isLoading && <button className='submit-btn'>LOGGING IN...</button>}
-                </form>
-                <p>Not a user yet? <button onClick={() => navigate("/signup")} className="redirect">Click here to signup.</button></p>
+                <Typography variant="h4" fontWeight={700} mb={4} letterSpacing={3}><LoginIcon style={{verticalAlign:"middle"}} sx={{mr: 1}} fontSize="large"/>LOGIN</Typography>
+                    <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column", my: 5}}>
+                        <TextField 
+                            id="username" 
+                            type="text" 
+                            label={<p><AccountCircleIcon style={{verticalAlign:"middle"}} sx={{mr: 1}}/>Username</p>} 
+                            value={username} 
+                            variant="outlined" 
+                            onChange={e => setUsername(e.target.value)}
+                            required
+                            sx={{my:2, minWidth: "320px"}}
+                        />
+                        <TextField 
+                            id="password" 
+                            type="password" 
+                            label={<p><KeyIcon style={{verticalAlign:"middle"}} sx={{mr: 1}}/>Password</p>} 
+                            value={password} 
+                            variant="outlined" 
+                            onChange={e => setPassword(e.target.value)}
+                            required
+                            sx={{my:2, minWidth: "320px"}}
+                        />
+                        {error && <Alert severity="error">{error}</Alert>}
+                        {!isLoading && <Button variant="contained" type="button" size="large" sx={{ mt: 5 }}>Login</Button>}
+                        {isLoading && <Button variant="contained" type="button" size="large" sx={{ mt: 5 }} disabled>LOGGING IN...</Button>}
+                    </form>
+                <Typography variant="body2" mt={4}>Not a member yet? <Button  to="/signup" LinkComponent={Link} sx={{textTransform: "none"}}>Click here to signup.</Button></Typography>
             </Paper>
         </Container>
     </PageContainer>
